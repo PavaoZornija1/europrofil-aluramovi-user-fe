34298a7206b02b6b72cc944097fa56a7bcfc8a7c
Nemanja Borojevic <38696838+NemanjaBorojevic@users.noreply.github.com>
2023-07-25 09:23:38 +0200

    removed unused props

--- app/rams/[id]/components/configurator/components/form/page.jsx
+++ app/rams/[id]/components/configurator/components/form/page.jsx
@@ -2,21 +2,14 @@
 
 import React from "react";
 import Step1 from "./step1/page";
+import Step2 from "./step2/page";
 
 function Form(props) {
   return (
     <>
       <form className="flex w-full flex-col gap-y-6">
-        {props.selectedStep === 1 && (
-          <Step1
-            height={props.height}
-            setHeight={props.setHeight}
-            width={props.width}
-            setWidth={props.setWidth}
-            handleOpenModal={props.handleOpenModal}
-            handleCloseModal={props.handleCloseModal}
-          />
-        )}
+        {props.selectedStep === 1 && <Step1 />}
+        {props.selectedStep === 2 && <Step2 />}
       </form>
     </>
   );
