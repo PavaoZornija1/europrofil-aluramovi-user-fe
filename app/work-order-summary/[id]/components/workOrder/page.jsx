1dc4f358781188be11bb97686bce43bba96f1ec5
Nemanja Borojevic <nele_borojevic@hotmail.com>
2023-09-04 03:35:44 -0700

    created pdf layout

--- app/work-order-summary/[id]/components/workOrder/page.jsx
+++ app/work-order-summary/[id]/components/workOrder/page.jsx
@@ -12,31 +12,6 @@ import PersonsInCharge from "./personsInCharge/page";
 const WorkOrder = () => {
   return (
     <div className="mt-4 flex w-full flex-col items-center gap-4 text-black">
-      <div className="flex w-full flex-col items-center sm:flex-row sm:justify-between">
-        <h3 className="w-full text-start text-2xl font-semibold uppercase tracking-wider sm:w-1/3">
-          Radni Nalog
-        </h3>
-        <div className="my-2 flex w-full flex-col items-center gap-2 sm:max-w-lg sm:flex-row">
-          <Link
-            className="w-full max-w-[320px] rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
-            href={"/mechanisms/1?step=1"}
-          >
-            Izmeni Nalog
-          </Link>
-          <div
-            className="w-full max-w-[320px] cursor-pointer rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
-            // onClick={createWorkOrderPdf}
-          >
-            Štampa
-          </div>
-          <Link
-            className="w-full max-w-[320px] rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
-            href={""}
-          >
-            Sačuvaj
-          </Link>
-        </div>
-      </div>
       <div className="w-full">
         <CustomerData />
         <Specifications />
--- app/work-order-summary/[id]/page.jsx
+++ app/work-order-summary/[id]/page.jsx
@@ -5,6 +5,7 @@ import WorkOrder from "./components/workOrder/page";
 import PurchaseOrder from "./components/purchaseOrder/page";
 import Navbar from "@/app/navbar/page";
 import { usePathname } from "next/navigation";
+import createWorkOrderPdf from "./components/pdf/pdf";
 
 function WorkOrderMain(props) {
   const [workOrderActive, setWorkOrderActive] = useState(true);
@@ -51,6 +52,31 @@ function WorkOrderMain(props) {
               }}
             >
               Porudžbenica
+            </Link>
+          </div>
+        </div>
+        <div className="flex w-full flex-col items-center sm:flex-row sm:justify-between text-black">
+          <h3 className="w-full text-start text-2xl font-semibold uppercase tracking-wider sm:w-1/3">
+            {workOrderActive ? "Radni nalog" : "Porudžbenica"}
+          </h3>
+          <div className="my-2 flex w-full flex-col items-center gap-2 sm:max-w-lg sm:flex-row">
+            <Link
+              className="w-full max-w-[320px] rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
+              href={"/rams/1?step=1"}
+            >
+              Izmeni Nalog
+            </Link>
+            <div
+              className="w-full max-w-[320px] cursor-pointer rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
+              onClick={createWorkOrderPdf}
+            >
+              Štampa
+            </div>
+            <Link
+              className="w-full max-w-[320px] rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
+              href={""}
+            >
+              Sačuvaj
             </Link>
           </div>
         </div>
