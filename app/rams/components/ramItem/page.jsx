cc6322975b40661af182ad394b4d82220a99b035
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-19 22:09:06 +0200

    minor fix

--- app/rams/components/ramItem/page.jsx
+++ app/rams/components/ramItem/page.jsx
@@ -6,7 +6,7 @@ function RamItem(props) {
   const { ram } = props;
   return (
     <div
-      key={ram.id}
+      key={ram?.id}
       className="flex flex-col p-4 items-center rounded-md border shadow-lg transition-all duration-200 hover:shadow-2xl"
     >
       <a
@@ -16,16 +16,16 @@ function RamItem(props) {
       >
         detaljnije
       </a>
-      <Link href={`/rams/${ram.id}?step=1`}>
+      <Link href={`/rams/${ram?.id}?step=1`}>
         <Image
-          src={`/images/${ram.name?.replace("/", "q")} app.jpeg`}
+          src={`/images/${ram?.name?.replace("/", "q")} app.jpeg`}
           alt="ram"
           width={250}
           height={250}
           className="bg-blue-100 h-[250px] w-[250px] flex justify-center items-center border mb-4"
         />
       </Link>
-      <h2 className="font-semibold mb-4">{ram.name}</h2>
+      <h2 className="font-semibold mb-4">{ram?.name}</h2>
       <div className="flex gap-12 border-t pt-2">
         <p className="cursor-pointer hover:underline">Tehnički crtež</p>
         <p className="cursor-pointer hover:underline">Galerija</p>
