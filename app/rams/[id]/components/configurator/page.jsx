c4f576c9b87dbbed2a80f43a70f2f724ea8c8e65
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-21 10:28:01 +0200

    passed prop

--- app/rams/[id]/components/configurator/page.jsx
+++ app/rams/[id]/components/configurator/page.jsx
@@ -6,7 +6,7 @@ export default function Configurator(props) {
     <>
       <div className="pb-24 text-black">
         <div className="m-auto flex w-11/12 flex-col justify-between gap-4 lg:flex-row">
-          <Form selectedStep={props.selectedStep} />
+          <Form selectedStep={props.selectedStep} ram={props.ram} />
         </div>
       </div>
     </>
