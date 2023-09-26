502bb07e7150c485fdb2af1897411a06742f7f5b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-26 13:41:05 +0200

    minor change

--- app/rams/[id]/components/configurator/components/form/step1/components/treatments/page.jsx
+++ app/rams/[id]/components/configurator/components/form/step1/components/treatments/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 function Treatments(props) {
   const { treatments, accordionFor } = props;
--- app/rams/[id]/components/configurator/components/form/step1/page.jsx
+++ app/rams/[id]/components/configurator/components/form/step1/page.jsx
@@ -7,32 +7,6 @@ import { Config } from "@/config";
 import Treatments from "./components/treatments/page";
 
 export default function Step1(props) {
-  const [fills, setFills] = useState([]);
-  const [treatments, setTreatments] = useState([]);
-  useEffect(() => {
-    const getFills = async () => {
-      try {
-        const response = await axios.get(`${Config.baseURL}/api/alu-fills/`);
-        setFills(response.data);
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    };
-    getFills();
-  }, []);
-  useEffect(() => {
-    const getTreatments = async () => {
-      try {
-        const response = await axios.get(
-          `${Config.baseURL}/api/alu-frame-treatments/`
-        );
-        setTreatments(response.data);
-      } catch (error) {
-        console.error("Error: ", error);
-      }
-    };
-    getTreatments();
-  }, []);
   return (
     <div className="w-full grid gap-8 grid-cols-1 lg:grid-cols-2">
       <div className="border border-sky-100 rounded-lg shadow-md shadow-gray-500 p-6">
