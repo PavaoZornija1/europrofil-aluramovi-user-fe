b443a95aa2e0c5730f4f17986bc6528a2bc360b3
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-27 10:42:07 +0200

    render redux data

--- app/work-order-summary/[id]/components/workOrder/specification/page.jsx
+++ app/work-order-summary/[id]/components/workOrder/specification/page.jsx
@@ -1,26 +1,38 @@
 "use client";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const Specifications = () => {
+  const frame = useSelector((state) => state.data.frameType);
+  const treatment = useSelector((state) => state.data.treatment);
+  const fill = useSelector((state) => state.data.fill);
+  const additionalTreatment = useSelector(
+    (state) => state.data.additionalFillTreatment
+  );
+  const ralCode = useSelector((state) => state.data.ralCode);
+  const subfill = useSelector((state) => state.data.subFill);
+
   return (
     <div className="flex flex-col border-t py-4">
       <h3 className="text-2xl font-semibold">Osnovna specifikacija</h3>
       <div className="flex flex-col gap-4 py-4 sm:flex-row sm:justify-evenly">
         <div className="flex gap-2">
           <span className="font-semibold">Tip rama:</span>
-          <span>RAM 1003 N</span>
+          <span>{frame?.name}</span>
         </div>
         <div className="flex gap-2">
           <span className="font-semibold">Obrada:</span>
-          <span>Antracit siva</span>
+          <span>
+            {treatment?.name} {ralCode && `(RAL ${ralCode})`}
+          </span>
         </div>
         <div className="flex gap-2">
           <span className="font-semibold">Ispuna:</span>
-          <span>Ogledalo obicno</span>
+          <span>{subfill?.name ? subfill?.name : fill?.name}</span>
         </div>
         <div className="flex gap-2">
           <span className="font-semibold">Dodatna obrada:</span>
-          <span>KP obrada ivica</span>
+          <span>{additionalTreatment}</span>
         </div>
       </div>
     </div>
