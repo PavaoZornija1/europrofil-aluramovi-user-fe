777b1dc8170314b89bab1be4f0a5cf9d90f565df
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-09-28 23:06:20 +0200

    minor change

--- app/work-order-summary/[id]/page.jsx
+++ app/work-order-summary/[id]/page.jsx
@@ -60,12 +60,12 @@ function WorkOrderMain(props) {
             {workOrderActive ? "Radni nalog" : "Porudžbenica"}
           </h3>
           <div className="my-2 flex w-full flex-col items-center gap-2 sm:max-w-lg sm:flex-row">
-            <Link
+            {/* <Link
               className="w-full max-w-[320px] rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
               href={"/rams/1?step=1"}
             >
               Izmeni Nalog
-            </Link>
+            </Link> */}
             <div
               className="w-full max-w-[320px] cursor-pointer rounded-md border bg-gradient-to-tr from-green-500 to-green-600 px-3 py-1 text-center text-lg font-semibold uppercase text-white transition-all duration-200 hover:brightness-125"
               onClick={createWorkOrderPdf}
