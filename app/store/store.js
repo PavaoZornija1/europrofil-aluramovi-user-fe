6490a26db856d46699665072273beae33dde6524
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-11 13:01:25 +0100

    implemented redux-persist logic

--- app/store/store.js
+++ app/store/store.js
@@ -1,8 +1,29 @@
-const { configureStore } = require("@reduxjs/toolkit");
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import ramDataReducer from "../features/ram/ramData";
+import storage from "redux-persist/lib/storage";
+import {
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
+const reducers = combineReducers({ data: ramDataReducer });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
 export default configureStore({
-  reducer: {
-    data: ramDataReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
