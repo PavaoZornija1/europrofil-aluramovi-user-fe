33b53c5584952cc122a2e758f7bf94bce5bea1c3
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-12 06:21:21 +0100

    created intiial layoput

---
+++ app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/liftSupport/LeftSupport.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const LeftSupport = ({ frame }) => {
+  return (
+    <>
+      {(frame.orientation === "Kip vrata" &&
+        frame.liftingSystem?.activePositionOption === 0) ||
+        (frame.liftingSystem?.activePositionOption === 2 && (
+          <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-white">
+            {frame.liftingSystem?.liftSupport?.name}
+          </div>
+        ))}
+    </>
+  );
+};
+
+export default LeftSupport;
