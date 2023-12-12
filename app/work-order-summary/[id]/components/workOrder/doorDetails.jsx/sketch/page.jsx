8d117bb5e7df9d6a58ec9eef0468c7c986ec21f4
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-12 06:21:42 +0100

    refactorization

--- app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/page.jsx
+++ app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/page.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import RenderHandleProfileDetails from "./renderProfile/page";
+import LeftSupport from "./liftSupport/LeftSupport";
+import RightSupport from "./liftSupport/RightSupport";
 
 const SketchDetails = ({ frame }) => {
   console.log(frame);
@@ -11,35 +14,23 @@ const SketchDetails = ({ frame }) => {
               Strana sa šarkama
             </div>
           )}
-          {(frame.orientation === "Kip vrata" &&
-            frame.liftingSystem?.activePositionOption === 0) ||
-            (frame.liftingSystem?.activePositionOption === 2 && (
-              <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-white">
-                {frame.liftingSystem?.liftSupport?.name}
-              </div>
-            ))}
+          <LeftSupport frame={frame} />
           {frame.orientation === "Desna vrata" && (
             <div className="[writing-mode:vertical-lr] ">
               {Number(frame.dimensions.height).toFixed(2)}
             </div>
           )}
         </div>
-        <div className="h-56 w-52 border-slate-400 border-2 bg-green-400">
-          {frame.orientation}
-        </div>
+        {/* MAIN CONTAINER */}
+        <RenderHandleProfileDetails frame={frame} />
+        {/* ******************** */}
         <div className="[writing-mode:vertical-lr] text-center">
           {frame.orientation === "Desna vrata" && (
             <div className="[writing-mode:vertical-lr] bg-gray-300 text-center uppercase tracking-wider text-white">
               Strana sa šarkama
             </div>
           )}
-          {(frame.orientation === "Kip vrata" &&
-            frame.liftingSystem?.activePositionOption === 1) ||
-            (frame.liftingSystem?.activePositionOption === 2 && (
-              <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-white">
-                {frame.liftingSystem?.liftSupport?.name}
-              </div>
-            ))}
+          <RightSupport frame={frame} />
           {frame.orientation === "Leva vrata" && (
             <div className="[writing-mode:vertical-lr] ">
               {Number(frame.dimensions.height).toFixed(2)}
