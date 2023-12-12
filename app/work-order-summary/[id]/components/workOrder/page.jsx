f1cdf22ae6577006f2c7977a01407ff1d91b18d9
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-12 03:49:39 +0100

    passed props

--- app/work-order-summary/[id]/components/workOrder/page.jsx
+++ app/work-order-summary/[id]/components/workOrder/page.jsx
@@ -7,17 +7,19 @@ import DoorDetails from "./doorDetails.jsx/page";
 
 // import createWorkOrderPdf from "./customerData/workOrderPdf/pdf";
 
-const WorkOrder = () => {
+const WorkOrder = ({ activeFrame, individualFronts }) => {
   return (
     <div className="mt-4 flex w-full flex-col items-center gap-4 text-black">
       <div className="w-full">
         <CustomerData />
         <Specifications />
         <PersonsInCharge />
-        <DoorDetails />
-        {/* <Rails />
-        <SchemaView />
-         */}
+        <DoorDetails
+          activeFrame={activeFrame}
+          individualFronts={individualFronts}
+        />
+        {/* <Rails /> */}
+        {/* <SchemaView /> */}
       </div>
     </div>
   );
