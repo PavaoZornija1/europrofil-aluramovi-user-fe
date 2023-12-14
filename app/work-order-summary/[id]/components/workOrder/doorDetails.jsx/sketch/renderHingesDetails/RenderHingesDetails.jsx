19f230c34a6767a19ff4a0c9efaf76acd1def423
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-14 01:54:40 +0100

    fix

--- app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/renderHingesDetails/RenderHingesDetails.jsx
+++ app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/renderHingesDetails/RenderHingesDetails.jsx
@@ -1,27 +1,40 @@
 import React from "react";
 
 const RenderHingesDetails = ({ frame }) => {
-  let orientation;
+  let position;
   if (frame.orientation === "Leva vrata") {
-    orientation = `left-0 h-full w-5`;
+    position = `left-0 h-full w-5`;
   } else if (frame.orientation === "Desna vrata") {
-    orientation = `right-0 h-full w-5`;
+    position = `right-0 h-full w-5`;
   } else if (frame.orientation === "Kip vrata") {
-    orientation = `top-0 w-full h-5 flex`;
+    position = `top-0 w-full h-5 flex`;
   }
   return (
-    <div className={`absolute ${orientation}`}>
+    <div className={`absolute ${position}`}>
       {frame.hinges?.centerDistanceOfHoles?.map((hole, index) => (
-        <div
-          key={index}
-          className={`border rounded-full w-4 h-4 bg-white absolute`}
-          style={{
-            [frame.orientation === "Leva vrata" ||
-            frame.orientation === "Desna vrata"
-              ? "bottom"
-              : "left"]: `${hole / 10 - 2.5}%`,
-          }}
-        ></div>
+        <>
+          <div
+            key={index}
+            className={`border rounded-full w-4 h-4 bg-white absolute`}
+            style={{
+              [frame.orientation === "Leva vrata" ||
+              frame.orientation === "Desna vrata"
+                ? "bottom"
+                : "left"]: `${hole / 10 - 2.5}%`,
+            }}
+          ></div>
+          <div
+            className="text-sm absolute"
+            style={{
+              [frame.orientation === "Leva vrata" ||
+              frame.orientation === "Desna vrata"
+                ? "bottom"
+                : "left"]: `${hole / 10 - 2.5}%`,
+            }}
+          >
+            {hole}
+          </div>
+        </>
       ))}
     </div>
   );
