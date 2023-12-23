7843b38d9c553c1603b1cd9bf5aabe49f22c72c1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-23 11:21:52 +0100

    css change

--- app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/liftSupport/RightSupport.jsx
+++ app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/liftSupport/RightSupport.jsx
@@ -6,7 +6,7 @@ const RightSupport = ({ frame }) => {
       {(frame.orientation === "Kip vrata" &&
         frame.liftingSystem?.activePositionOption === 1) ||
         (frame.liftingSystem?.activePositionOption === 2 && (
-          <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-white">
+          <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-black">
             {frame.liftingSystem?.liftSupport?.name}
           </div>
         ))}
