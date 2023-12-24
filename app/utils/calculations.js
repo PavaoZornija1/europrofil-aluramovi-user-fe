73b478173ddeae16d3c2f98b5257e5567a3f4e6b
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-24 01:19:05 +0100

    added calculations

---
+++ app/utils/calculations.js
@@ -0,0 +1,75 @@
+export const calculateAluFrameSurfaces = (fronts, treatment) => {
+  let percentage = Number(treatment.priceIncrease) / 100;
+  let result = 0;
+
+  for (let i = 0; i < fronts.length; ++i) {
+    let loopResult = 0;
+    if (fronts[i].dimensions?.height !== fronts[i].dimensions?.width) {
+      loopResult =
+        fronts[i].dimensions?.numberOfPieces *
+        (2 *
+          (Number(fronts[i].dimensions?.width) +
+            Number(fronts[i].dimensions?.height)) +
+          2 *
+            (Number(fronts[i].dimensions?.width) +
+              Number(fronts[i].dimensions?.height)) *
+            percentage);
+    } else {
+      loopResult =
+        fronts[i].dimensions?.numberOfPieces *
+        (4 * fronts[i].dimensions?.width +
+          4 * fronts[i].dimensions?.width * percentage);
+    }
+
+    result += loopResult;
+  }
+
+  return result / 1000;
+};
+
+export const calculateFillHeight = (frontHeight, frame) => {
+  return frontHeight - 2 * frame.fillingHeightReduction * 10;
+};
+
+export const calculateAluFrameFillSurfaces = (frame, fronts, fill) => {
+  const percentage =
+    Number(fill?.priceIncrease) / 100 ||
+    Number(fill?.subfill?.priceIncrease) / 100;
+  let result = 0;
+  for (let i = 0; i < fronts.length; ++i) {
+    result +=
+      (calculateFillHeight(fronts[i].dimensions?.height, frame) *
+        fronts[i].dimensions?.width *
+        fronts[i].dimensions?.numberOfPieces +
+        calculateFillHeight(fronts[i].dimensions?.height, frame) *
+          fronts[i].dimensions?.width *
+          fronts[i].dimensions?.numberOfPieces *
+          percentage) /
+      (1000 * 1000);
+  }
+  return result;
+};
+export const calculateAdditionalFillTreatment = (frame, fronts, fill) => {
+  let result = 0;
+  for (let i = 0; i < fronts.length; ++i) {
+    result +=
+      (calculateFillHeight(fronts[i].dimensions?.height, frame) *
+        fronts[i].dimensions?.width *
+        fronts[i].dimensions?.numberOfPieces) /
+      (1000 * 1000);
+  }
+  return result;
+};
+
+export const calculateMetalCornersQuantity = (fronts) => {
+  let totalCorners = 0;
+
+  for (let i = 0; i < fronts.length; ++i) {
+    totalCorners += fronts[i].dimensions?.numberOfPieces;
+  }
+  return totalCorners * 4;
+};
+
+// export const calculateHandleProfileLength = (front) => {
+//     return front.handles
+// }
