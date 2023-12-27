2b2c2a09094d9e4cab3811ee4df89876e7c2ffaf
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-27 08:12:25 +0100

    fix

--- app/rams/[id]/components/configurator/components/form/step2/components/sketch/components/locksRenderer/page.jsx
+++ app/rams/[id]/components/configurator/components/form/step2/components/sketch/components/locksRenderer/page.jsx
@@ -1,48 +1,51 @@
 "use client";
-import React, { useState, useEffect } from "react";
+
+import { useSelector } from "react-redux";
 
 function LocksRenderer(props) {
   const { frontsData, activeFrontId, dimensions } = props;
-
+  const individualFronts = useSelector((state) => state.data.individualFronts);
   return (
     <div
       style={{
         position: "absolute",
         top: `calc(100% - ${
           ((100 / dimensions.h) *
-            (frontsData[activeFrontId].locks.holeDiameter / 10)) /
+            (individualFronts[activeFrontId].locks.holeDiameter / 10)) /
             2 +
           "%"
         } - ${
           (100 / dimensions.h) *
-            frontsData[activeFrontId].locks.centerDistanceOfHole[0] +
+            individualFronts[activeFrontId].locks.centerDistanceOfHole[0] +
           "%"
         })`,
         left:
-          frontsData[activeFrontId].orientation === "Leva vrata"
+          individualFronts[activeFrontId].orientation === "Leva vrata"
             ? `calc(100% - ${
                 ((100 / dimensions.h) *
-                  (frontsData[activeFrontId].locks.holeDiameter / 10)) /
+                  (individualFronts[activeFrontId].locks.holeDiameter / 10)) /
                   2 +
                 "%"
               } - ${
                 (100 / dimensions.h) *
-                  frontsData[activeFrontId].locks.centerDistanceOfHole[1] +
+                  individualFronts[activeFrontId].locks
+                    .centerDistanceOfHole[1] +
                 "%"
               })`
             : `calc(0% - ${
                 ((100 / dimensions.h) *
-                  (frontsData[activeFrontId].locks.holeDiameter / 10)) /
+                  (individualFronts[activeFrontId].locks.holeDiameter / 10)) /
                   2 +
                 "%"
               } +  ${
                 (100 / dimensions.h) *
-                  frontsData[activeFrontId].locks.centerDistanceOfHole[1] +
+                  individualFronts[activeFrontId].locks
+                    .centerDistanceOfHole[1] +
                 "%"
               })`,
         height:
           (100 / dimensions.h) *
-            (frontsData[activeFrontId].locks.holeDiameter / 10) +
+            (individualFronts[activeFrontId].locks.holeDiameter / 10) +
           "%",
       }}
       className="border border-gray-800 aspect-square rounded-full bg-white"
