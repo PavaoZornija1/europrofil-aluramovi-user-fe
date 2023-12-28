b20977654a5763c1756c62d5dd7220275689eeb0
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2023-12-29 00:40:27 +0100

    fix

--- app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/liftSupport/RightSupport.jsx
+++ app/work-order-summary/[id]/components/workOrder/doorDetails.jsx/sketch/liftSupport/RightSupport.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const RightSupport = ({ frame }) => {
   return (
     <>
-      {(frame.orientation === "Kip vrata" &&
+      {((frame.orientation === "Kip vrata" &&
         frame.liftingSystem?.activePositionOption === 1) ||
-        (frame.liftingSystem?.activePositionOption === 2 && (
-          <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-black">
-            {frame.liftingSystem?.liftSupport?.name}
-          </div>
-        ))}
+        frame.liftingSystem?.activePositionOption === 2) && (
+        <div className="[writing-mode:vertical-lr] bg-blue-300 text-center uppercase tracking-wider text-black">
+          {frame.liftingSystem?.liftSupport?.name}
+        </div>
+      )}
     </>
   );
 };
