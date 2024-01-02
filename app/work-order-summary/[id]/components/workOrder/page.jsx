33cb5ba92302e9d1bc9fe570eeffb55b660e413a
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-01-03 00:09:25 +0100

    added loading state

--- app/work-order-summary/[id]/components/workOrder/page.jsx
+++ app/work-order-summary/[id]/components/workOrder/page.jsx
@@ -4,23 +4,28 @@ import CustomerData from "./customerData/page";
 import Specifications from "./specification/page";
 import PersonsInCharge from "./personsInCharge/page";
 import DoorDetails from "./doorDetails.jsx/page";
+import Loading from "@/app/loading";
 
 // import createWorkOrderPdf from "./customerData/workOrderPdf/pdf";
 
-const WorkOrder = ({ activeFrame, individualFronts }) => {
+const WorkOrder = ({ activeFrame, individualFronts, isLoading }) => {
   return (
     <div className="mt-4 flex w-full flex-col items-center gap-4 text-black">
-      <div className="w-full">
-        <CustomerData />
-        <Specifications />
-        <DoorDetails
-          activeFrame={activeFrame}
-          individualFronts={individualFronts}
-        />
-        <PersonsInCharge />
-        {/* <Rails /> */}
-        {/* <SchemaView /> */}
-      </div>
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <div className="w-full">
+          <CustomerData />
+          <Specifications />
+          <DoorDetails
+            activeFrame={activeFrame}
+            individualFronts={individualFronts}
+          />
+          <PersonsInCharge />
+          {/* <Rails /> */}
+          {/* <SchemaView /> */}
+        </div>
+      )}
     </div>
   );
 };
