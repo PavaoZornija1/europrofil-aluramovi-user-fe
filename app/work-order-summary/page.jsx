ab0c253fe910fe9c3900d877a80a8fed6f44bed1
VanjaXYZ <vanja.sukurma.1991@gmail.com>
2024-01-03 00:09:06 +0100

    passed props

--- app/work-order-summary/page.jsx
+++ app/work-order-summary/page.jsx
@@ -1,11 +1,18 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import WorkOrderMain from "./[id]/page";
 
 function WorkOrderSummary(props) {
+  const [showSavedButton, setShowSavedButton] = useState(false);
+  const [showMoreActionButtons, setShowMoreActionButtons] = useState(false);
   return (
     <div>
-      <WorkOrderMain />
+      <WorkOrderMain
+        showSavedButton={showSavedButton}
+        setShowSavedButton={setShowSavedButton}
+        showMoreActionButtons={showMoreActionButtons}
+        setShowMoreActionButtons={setShowMoreActionButtons}
+      />
     </div>
   );
 }
